837d7f542fc44b5562753b745ce3e004e4efc7a7
AlexeyMilkh <107917572+AlexeyMilkh@users.noreply.github.com>
2022-06-24 16:12:00 +0300

    Update [hw]03_task_string.py
    Решение основано не на методах, но другого решения найти не смог(

--- Module3/home_work/string/[hw]03_task_string.py
+++ Module3/home_work/string/[hw]03_task_string.py
@@ -1,3 +1,10 @@
 # Посчитайте количество согласных букв в данной строке.
 
-text = ...
+text = input("Put your text here:")
+sgl = "бвгджзйклмнпрстфхцчшщ"
+count = 0
+
+for i in text:
+    if i in sgl:
+        count += 1
+print(count)
