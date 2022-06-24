5a5843cf25b8e9e0009b9f0f01130dbbec4b4175
AlexeyMilkh <107917572+AlexeyMilkh@users.noreply.github.com>
2022-06-24 15:11:18 +0300

    Update [hw]01_task_string.py

--- Module3/home_work/string/[hw]01_task_string.py
+++ Module3/home_work/string/[hw]01_task_string.py
@@ -1,4 +1,11 @@
 # Дан произвольный текст. Каких символов больше, точек "." или запятых ","?
 # Если точек и запятых одинаково - выведите "одинаково"
 
-text = ...
+text = input("Put your text here:")
+
+if text.count(",") > text.count("."):
+    print("More: ,")
+elif text.count(".") > text.count(","):
+    print("More: .")
+else:
+    print("=")
