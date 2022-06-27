cdacffb7f79211e190d033c4e115cd1a6cbbb800
AlexeyMilkh <107917572+AlexeyMilkh@users.noreply.github.com>
2022-06-27 19:53:26 +0300

    Update 06_task_list.py

--- Module3/practice/list_and_tuple/06_task_list.py
+++ Module3/practice/list_and_tuple/06_task_list.py
@@ -5,3 +5,10 @@ first_number = int(input())     # Первое число
 second_number = int(input())    # Второе число
 
 # TODO: your code here
+
+a = int(input("Put number: "))
+b = int(input("Put number: "))
+
+for i in range(a,b):
+    if i % 3 == 0:
+        print(i, end=" ")
