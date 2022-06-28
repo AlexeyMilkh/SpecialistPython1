1852a217b2e970738750b65f81214135d89a41c0
AlexeyMilkh <107917572+AlexeyMilkh@users.noreply.github.com>
2022-06-28 14:38:35 +0300

    Update 03_hw_func.py

--- Module4/home_work/03_hw_func.py
+++ Module4/home_work/03_hw_func.py
@@ -2,3 +2,19 @@
 # Находится ли одна окружность целиком внутри другой
 
 # TODO: your code here
+
+def circle_in(x1, y1):
+    if (r2 - r1)**2 > (x2 - x1)**2 + (y2 - y1)**2:
+        return "Входит"
+    else:
+        return "Не входит"
+
+
+x1 = int(input("X1:"))
+y1 = int(input("Y1:"))
+x2 = int(input("X2:"))
+y2 = int(input("Y2:"))
+r1 = int(input("R1"))
+r2 = int(input("R2:"))
+
+print(circle_in(x1, y1))
