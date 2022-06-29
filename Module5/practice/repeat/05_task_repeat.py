960f9462a8d1feaa142663390b82e9cf7b95bc2d
AlexeyMilkh <107917572+AlexeyMilkh@users.noreply.github.com>
2022-06-29 15:17:51 +0300

    Update 05_task_repeat.py

--- Module5/practice/repeat/05_task_repeat.py
+++ Module5/practice/repeat/05_task_repeat.py
@@ -1,3 +1,12 @@
 # Дано общее количество объектов для отображения на веб-сайте и количество объектов на каждой странице.
 # Вычислите, сколько объектов будет отображаться на последней странице сайта.
 
+def remainder(num_items, items_on_page):
+    return num_items - items_on_page * (num_items // items_on_page)
+
+
+num_items = int(input("Num item: "))
+items_on_page = int(input("Items on page: "))
+
+
+print(remainder(num_items, items_on_page))
